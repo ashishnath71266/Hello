bc7fd5d9e3cb5b34e5cc33ccb381aa0ded1f4999
ashishnath71266 <ashishnath581@gmail.com>
2018-05-25 13:35:00 +0530

    Initial commit

---
+++ app/src/main/java/com/codekul/app/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.codekul.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnOkay.setOnClickListener {
+           Log.i("@codekul", "Clicked")
+        }
+    }
+}
+
